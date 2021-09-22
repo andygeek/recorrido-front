2c650b73b6d661c7da8ee2d8283788cce7769e1f
Andy Eulogio <andy.eulogio@gmail.com>
2021-09-21 20:12:52 -0500

    Implementing a basic sign up

--- src/services/BackService.ts
+++ src/services/BackService.ts
@@ -1,20 +1,25 @@
 import axios, { AxiosInstance } from 'axios';
 
+interface LoginParams {
+  username: string,
+  password: string,
+}
+
 export default class BackService {
   apiBack : AxiosInstance;
 
   constructor () {
     this.apiBack = axios.create({
-      baseURL: '...',
+      baseURL: 'http://127.0.0.1:3000',
       withCredentials: false,
       headers: {
-        Accept: 'applicacion/json',
-        'Content-Type': 'application/jso',
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       }
     })
   }
 
-  firstRequest() {
-    return this.apiBack.get('/..')
+  signup(login_params: LoginParams) {
+    return this.apiBack.post('/users/signup', login_params)
   }
 }
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,12 +1,53 @@
 <template>
   <div class="signup-view__container">
-    <h1>Aqui te puedes registrar</h1>
+    <div>
+      <p>name:</p>
+      <input type="text" v-model="name">
+    </div>
+    <div>
+      <p>username:</p>
+      <input type="text" v-model="username">
+    </div>
+    <div>
+      <p>password</p>
+      <input type="text" v-model="password">
+    </div>
+    <button @click="signup" style="margin-top: 30px;">Registrarse</button>
+
+    <p>name: {{name}}</p>
+    <p>username: {{username}}</p>
+    <p>password: {{password}}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import BackService from '@/services/BackService'
 
 @Component({})
-export default class Signup extends Vue {}
+export default class Signup extends Vue {
+  name: string = ''
+  username: string = ''
+  password: string = ''
+
+  service : BackService | null = null
+
+  created() {
+    this.service = new BackService();
+  }
+
+  async signup() {
+    console.log(this.name, this.username, this.password);
+    try {
+      let body = {
+        name: this.name,
+        username: this.username,
+        password: this.password
+      }
+      let user = await this.service!.signup(body)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
 </script>
