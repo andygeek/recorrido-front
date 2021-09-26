ac00d1d132dafcfc859fa61fd5f0e33416423bd6
Andygeek <andy.eulogio@gmail.com>
2021-09-25 23:26:13 -0500

    Add Create Alert Modal component

---
+++ src/components/CreateAlertModal.vue
@@ -0,0 +1,96 @@
+<template>
+  <transition name="modal--fade">
+    <div class="create-modal__backdrop"  v-if="modalOpen">
+      <div
+        class="create-modal__container"
+        role="dialog"
+      >
+        <header class="create-modal__header">
+          <div class="create-modal__title">Crear alerta de precio</div>
+          <button type="button" class="create-modal__button-close" @click="closeModal">X</button>
+        </header>
+        <div class="create-modal__body">
+          <h1>ddd</h1>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class CreateAlertModal extends Vue {
+  @Prop({ type: Boolean, required: true }) open !: boolean
+  @Prop({ type: String, required: false }) title !: String
+
+  get modalOpen () {
+    return this.open
+  }
+
+  set modalOpen (v : boolean) {
+    this.$emit('update:open', v)
+  }
+
+  closeModal() {
+    this.modalOpen = false
+  }
+}
+</script>
+<style scoped>
+.create-modal__backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.create-modal__container {
+  width: 600px;
+  right: auto;
+  left: auto;
+  top: auto;
+  bottom: auto;
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+.create-modal__header {
+  margin-top: 30px;
+  margin-bottom: 10px;
+  position: relative;
+  display: flex;
+}
+.create-modal__title {
+  font-size: 25px;
+  position: relative;
+  width: 100%;
+  text-align: start;
+  font-family: system-ui;
+}
+.create-modal__button-close {
+  right: 0px;
+  position: absolute;
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+  font-weight: bold;
+  background: transparent;
+  outline: none;
+}
+.create-modal__body {
+  display: flex;
+  position: relative;
+  padding: 20px 10px;
+}
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,31 +1,34 @@
 <template>
   <div class="dashboard-view__container">
     <div class="dashboard_view__content">
-      <h1>Hola {{this.$store.state['auth'].user.name}}</h1>
       <h2>En esta web puedes crear alerta de precios para cualquier viaje que quieras hacer en bus.</h2>
     </div>
-    <div>
-      <input placeholder="name" type="text">
-    </div>
-    <button>Crear alerta de precios</button>
+    <button @click="openModal">Crear alerta de precios</button>
     <div class="alert-price__table">
       <div>alerta de precios: 111</div>
-    </div>  
+    </div>
+    <create-alert-modal :open.sync="createModalOpen"></create-alert-modal>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import CreateAlertModal from '@/components/CreateAlertModal.vue'
 
-@Component({})
+@Component({
+  components: {
+    CreateAlertModal
+  }
+})
 export default class Dashboard extends Vue {
-  name : string = ''
-  originId : number = 0
-  destinyId : number = 0
-  classId : number = 0
-  price : number = 0.0
 
+  createModalOpen : boolean = false
   listAlerts = []
+
+
+  openModal() {
+    this.createModalOpen = true
+  }
 
   created() {
     let token = this.$store.state["auth"].token
