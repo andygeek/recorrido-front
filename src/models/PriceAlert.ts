0e173f7abf5cc5dd1c39c741485f3b12dc6e0e87
Andygeek <andy.eulogio@gmail.com>
2021-09-26 23:15:45 -0500

    Add name and url_name in PriceALert interface

--- src/models/PriceAlert.ts
+++ src/models/PriceAlert.ts
@@ -1,9 +1,13 @@
 export interface PriceAlert {
   id?: number
   name: string
-  origin_id: number,
-  destiny_id: number,
-  class_id: number,
-  price: number,
+  origin_id: number
+  origin_name: string
+  origin_url_name: string
+  destiny_id: number
+  destiny_name: string
+  destiny_url_name: string
+  class_id: number
+  price: number
   user_id: number
 }
